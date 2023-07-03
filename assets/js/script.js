4b7fea578cdff5666a656eb287fc310f7e2a24bd
Marc-Hanson <>
2023-07-03 15:15:43 +0000

    Random number generated and images now change.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
-let randomNumber1 = math.random(math.floor * 6);
-let player = document.getElementsByClassName("player-choice");
-player.setAttribute()
+// Generates your random number and changes image. //
+const playersDice = Math.floor(Math.random() * 6) + 1;
+const player = document.getElementById("player-choice");
+player.setAttribute("src", "/assets/images/dice" + playersDice + ".webp");
 
-console.log(randomNumber1)
+//Generates opponents random number and changes dice image. //
+const opponentsDice = Math.floor(Math.random() * 6) + 1;
+const opponent = document.getElementById("opponent-choice");
+opponent.setAttribute("src", "/assets/images/dice" + opponentsDice + ".webp");
