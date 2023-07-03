64711f764e6eb831c105725dfea4f010174a8b06
Marc-Hanson <>
2023-07-03 11:03:53 +0000

    Images added to landing  page and styling complete.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
-console.log("Working");
+let randomNumber1 = math.random(math.floor * 6);
+let player = document.getElementsByClassName("player-choice");
+player.setAttribute()
+
+console.log(randomNumber1)
