9d40112ff068d8178300067d0e66ab6759c9b831
Marc-Hanson <>
2023-07-03 17:19:21 +0000

    Javascript logic finished, Device scaling added.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,16 @@ const playersDice = Math.floor(Math.random() * 6) + 1;
 const player = document.getElementById("player-choice");
 player.setAttribute("src", "/assets/images/dice" + playersDice + ".webp");
 
-//Generates opponents random number and changes dice image. //
+// Generates opponents random number and changes dice image. //
 const opponentsDice = Math.floor(Math.random() * 6) + 1;
 const opponent = document.getElementById("opponent-choice");
 opponent.setAttribute("src", "/assets/images/dice" + opponentsDice + ".webp");
+
+// Determines winner. //
+if (playersDice > opponentsDice) {
+    document.querySelector('h1').innerHTML = "You win!"
+} else if(opponentsDice > playersDice) {
+    document.querySelector('h1').innerHTML = "You Lose!"
+} else {
+    document.querySelector('h1').innerHTML = "It's a draw!"
+}
