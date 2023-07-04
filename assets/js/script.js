750d4acea218f61a0d0806d278c85f13e1a193a8
Marc-Hanson <>
2023-07-04 06:52:54 +0000

    Fixed missing images and altered color palette.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 // Generates your random number and changes image. //
 const playersDice = Math.floor(Math.random() * 6) + 1;
 const player = document.getElementById("player-choice");
-player.setAttribute("src", "/assets/images/dice" + playersDice + ".webp");
+player.setAttribute("src", "assets/images/dice" + playersDice + ".webp");
 
 // Generates opponents random number and changes dice image. //
 const opponentsDice = Math.floor(Math.random() * 6) + 1;
 const opponent = document.getElementById("opponent-choice");
-opponent.setAttribute("src", "/assets/images/dice" + opponentsDice + ".webp");
+opponent.setAttribute("src", "assets/images/dice" + opponentsDice + ".webp");
 
 // Determines winner. //
 if (playersDice > opponentsDice) {
